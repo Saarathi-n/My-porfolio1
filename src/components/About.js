e2dd2a622336172797ab31c07e46be1fa1654112
Saarathi-n <saarathin@gmail.com>
2025-10-21 19:43:19 +0530

    deploy test

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ const About = () => {
   return (
     <section className="section">
       <h2>About Me</h2>
-      <p>I'm Saarathi N, a passionate Full Stack Developer with expertise in building scalable web applications using React, Node.js, and modern databases. I enjoy creating user-friendly, visually appealing solutions with a focus on performance and innovation.</p>
+      <p>I'm Saarathi N, Developer with expertise in building scalable web applications using React, Node.js, and modern databases. I enjoy creating user-friendly, visually appealing solutions with a focus on performance and innovation.</p>
     </section>
   );
 };
