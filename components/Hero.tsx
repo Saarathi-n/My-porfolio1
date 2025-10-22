91d836aee46f0d7dda160b75bc7efda5c294e922
Saarathi-n <saarathin@gmail.com>
2025-10-22 11:25:57 +0530

    save changes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,13 @@
 
 import React from 'react';
+import Galaxy from './Galaxy';
 
 const Hero: React.FC = () => {
   return (
-    <section id="home" className="min-h-screen flex items-center justify-center">
+    <section id="home" className="min-h-screen flex items-center justify-center" style={{ position: 'relative' }}>
+      <div style={{ width: '100%', height: '100%', position: 'absolute', top: 0, left: 0, zIndex: -1 }}>
+        <Galaxy />
+      </div>
       <div className="text-center max-w-3xl">
         <h1 className="text-5xl md:text-7xl font-extrabold text-white mb-4">
           Hi, I'm <span className="text-cyan-400">Saarathi</span>
